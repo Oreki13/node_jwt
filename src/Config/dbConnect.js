889d7f4071c7dc1273d506fb233c438217671723
Oreki13 <arfandynugraha21@gmail.com>
2019-09-27 06:36:37 +0700

    make it better

--- src/Config/dbConnect.js
+++ src/Config/dbConnect.js
@@ -2,10 +2,10 @@ require("dotenv/config");
 const mysql = require("mysql");
 
 const dbConfig = mysql.createConnection({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE || "store_v2"
+  host: process.env.DB_HOST || "remotemysql.com",
+  user: process.env.DB_USER || "uCVrrTvHJf",
+  password: process.env.DB_PASSWORD || "OS6klfusg4",
+  database: process.env.DB_DATABASE || "uCVrrTvHJf"
 });
 
 dbConfig.connect(error => {
