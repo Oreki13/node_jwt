8a0491c0719bcf59f9b6b94a6eca871ba086b22c
Oreki13 <arfandynugraha21@gmail.com>
2019-09-28 19:47:18 +0700

    update

--- src/Config/dbConnect.js
+++ src/Config/dbConnect.js
@@ -2,10 +2,10 @@ require("dotenv/config");
 const mysql = require("mysql");
 
 const dbConfig = mysql.createConnection({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE || "store_v2"
+  host: process.env.DB_HOST || "remotemysql.com",
+  user: process.env.DB_USER || "uCVrrTvHJf",
+  password: process.env.DB_PASSWORD || "OS6klfusg4",
+  database: process.env.DB_DATABASE || "uCVrrTvHJf"
 });
 
 dbConfig.connect(error => {
@@ -13,3 +13,12 @@ dbConfig.connect(error => {
 });
 
 module.exports = dbConfig;
+
+// DB_HOST = 'localhost'
+// DB_USER = 'root'
+// DB_PASSWORD = ''
+// DB_DATABASE = 'store_v2'
+// PORT = 8080
+// NODE_ENV = 'Devploment'
+// SECRET_KEY = "skuy"
+// REQUEST_HEADERS= 'soul'
